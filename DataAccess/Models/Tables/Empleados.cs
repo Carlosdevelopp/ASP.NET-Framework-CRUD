5fce1b7758b521aaa4d493bf795191a5736a5a59
carlosdev <carlosdevelopp@gmail.com>
2025-04-03 01:52:09 -0600

    ActualizarEntidad

--- DataAccess/Models/Tables/Empleados.cs
+++ DataAccess/Models/Tables/Empleados.cs
@@ -4,15 +4,14 @@ namespace DataAccess.Models.Tables
 {
     public class Empleados
     {
-        public int Id { get; set; } 
+        public int EmpleadoId { get; set; } 
         public string Nombre { get; set; }
         public string ApellidoPaterno { get; set; }
         public string ApellidoMaterno { get; set; }
         public DateTime FechaNacimiento { get; set; }
         public DateTime FechaContratacion { get; set; }
         public string Telefono { get; set; }    
-        public string Correo { get; set; }    
-        public string Cargo { get; set; }    
+        public string Correo { get; set; }     
         public bool Estado { get; set; }    
         public int CargoId { get; set; }
         public int DepartamentoId { get; set; }
