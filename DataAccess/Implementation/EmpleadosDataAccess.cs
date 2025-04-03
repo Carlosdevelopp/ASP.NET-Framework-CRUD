0863013da1f0e0b07c86f3315a66fd75d3c5f9d5
carlosdev <carlosdevelopp@gmail.com>
2025-04-02 23:50:19 -0600

    AgregarMetodoGet

--- DataAccess/Implementation/EmpleadosDataAccess.cs
+++ DataAccess/Implementation/EmpleadosDataAccess.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using DataAccess.Models.Tables;
 
 namespace DataAccess.Implementation
 {
     public class EmpleadosDataAccess
     {
+        #region GET
+        //Método para obtener una lista de rsgistros
+        public List<Empleados> GetEmpleados()
+        {
+            return new List<Empleados>();
+        }
+        #endregion
+
     }
 }
